afa40839ba179362dc387ff447e3a1ba9c720df0
João Antonio Soares dos Passos <jdospassos8@gmail.com>
2022-03-21 00:19:13 -0300

    commit-cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 var express = require("express")
 var app = express()
-
+const cors = require("cors")
 const rotas = require("./routes/controller")
 const port = 3000
+
+app.use(cors())
 
 app.use("chat", rotas)
 
