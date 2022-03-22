ce39433a1fe3b1ab14f0cc4153d8122852a598fa
João Antonio Soares dos Passos <jdospassos8@gmail.com>
2022-03-22 17:21:47 -0300

    path-adjust

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 var express = require("express")
 var app = express()
+var cors = require("cors")
 const rotas = require("./routes/controller")
-const port = 3000
-const path = require("path")
+const port = 4300
+
+app.use(cors())
 
 app.use(express.json())
 
-app.use("chat", express.static(path.join(__dirname, "public")))
+app.use("", rotas)
 
 app.listen(port)
--- src/routes/controller.js
+++ src/routes/controller.js
@@ -2,16 +2,16 @@ const express = require("express")
 const getConvDao = require("../db/conversas_dao")
 const routes = express.Router()
 
-routes.get("conversas/:id", async (req, res) => {
+routes.get("/conversas/:id", async (req, res) => {
+  console.log("era pra bater aqui essa merda")
   const idParam = +req.params.id
   let result = await getConvDao.getConversas(idParam)
   res.json(result)
 })
 routes.get("/:id", (req, res) => {
-  console.log("era pra bater aqui essa merda")
-  res.send("Conversa de id" + req.params.id)
+  res.send("Conversa de id " + req.params.id)
 })
-// routes.post("login", (req, res) => {})
-// routes.post("message", (req, res) => {})
+routes.post("login", (req, res) => {})
+routes.post("message", (req, res) => {})
 
 module.exports = routes
